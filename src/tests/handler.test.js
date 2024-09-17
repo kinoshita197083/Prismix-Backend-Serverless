9b829702881b31d991c42bc8c9eca5a602075517
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2024-09-17 16:13:44 +1000

    test setup

--- src/tests/handler.test.js
+++ src/tests/handler.test.js
@@ -1,9 +1,9 @@
-const { handler } = require('../src/handlers/handler');
+const { hello } = require('../handlers/example');
 
 test('hello function returns correct response', async () => {
     const event = {};
-    const response = await handler(event);
+    const response = await hello(event);
     expect(response.statusCode).toBe(200);
     const body = JSON.parse(response.body);
-    expect(body.message).toBe('Hello from Serverless!');
+    expect(body.message).toBe('Hello from serverless!');
 });
